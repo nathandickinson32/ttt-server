6ec5d130763e22e802ee1a1589fca9e1478d2186
nathandickinson32 <nathandickinson32@gmail.com>
2025-10-02 15:30:56 -0400

    set up speclj, connected to both server and ttt libraries, setup abstraction for handlers and have a connection to a page with html using hiccup

---
+++ ttt-server/spec/ttt_server/core_spec.clj
@@ -0,0 +1,9 @@
+(ns ttt-server.core-spec
+  (:require [speclj.core :refer :all]
+            [ttt-server.core :refer :all]))
+
+(describe "Core"
+
+  (it "equals 1"
+    (should= 1 0))
+  )
--- ttt-server/src/ttt_server/core.clj
+++ ttt-server/src/ttt_server/core.clj
@@ -1,6 +1,73 @@
-(ns ttt-server.core)
+(ns ttt-server.core
+  (:gen-class)
+  (:require [hiccup2.core :as h])
+  (:import [server HttpServer Request Response]
+           [handlers RequestHandler HelloHandler]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn html-response [html]
+  (doto (Response.)
+    (.setStatusCode 200)
+    (.setStatusMessage "OK")
+    (.addHeader "Content-Type" "text/html")
+    (.setBody html)))
+
+(def status-messages
+  {200 "OK"
+   404 "Not Found"})
+
+(defn set-headers [response headers]
+  (doseq [[k v] headers]
+    (.addHeader response k v)))
+
+(defn Request->map [^Request request]
+  {:method (.getMethod request)
+   :params (.getParams request)})
+
+(defn ->Response [response]
+  (doto (Response.)
+    (.setStatusCode (:status response))
+    (.setStatusMessage (get status-messages (:status response)))
+    (.setBody (:body response))
+    (set-headers (:headers response))))
+
+(defn ->request-handler [handler]
+  (proxy [RequestHandler] []
+    (handle [^Request request]
+      (-> request Request->map handler ->Response))))
+
+(defn fallback-handler-2 [_request]
+  {:status 404
+   :body   "Route not found"})
+
+(defn fallback-handler []
+  (proxy [RequestHandler] []
+    (handle [_request]
+      (doto (Response.)
+        (.setStatusCode 404)
+        (.setStatusMessage "Not Found")
+        (.setBody "Route not found")))))
+
+(defn index-handler-2 [_request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str (h/html [:h1 "Welcome to Tic Tac Toe"]))})
+
+(defn index-handler []
+  (proxy [RequestHandler] []
+    (handle [_request]
+      (html-response "<h1>Welcome to Tic Tac Toe</h1>"))))
+
+(defn register-routes [^HttpServer server]
+  (.addHandler server "GET" "/"
+               (->request-handler index-handler-2)
+               ;(index-handler)
+               ))
+
+(defn -main [& _]
+  (let [
+        ;fallback (fallback-handler)
+        fallback (->request-handler fallback-handler-2)
+        server   (HttpServer. 8081 fallback)]
+    (register-routes server)
+    (println "TTT server running on http://localhost:8081")
+    (.start server)))
